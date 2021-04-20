06080fa13f046e6659770c851246345ec30bf2d7
Kiah Pleasant <kiahpleasant@gmail.com>
2021-04-20 19:33:11 -0400

    push gh

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function createParagraph () {
 
 
 
-    var paragraph = '<p> Rick is the smartest' + nounOne + ' in the galaxy, but he\'s also the most ' + adjectiveOne + '. He uses his intelligence for all the ' + adjectiveTwo + ' reasons, such as turning himself into a talking ' + foodOne + ' to avoid going to (the) ' + placeOne + ' with his family. He won\'t come to the rescue when some ' + pluralNounOne + ' are nibbling on my ' + bodyPartOne + ' unless there\'s something in it for him, and he definitely won\'t care if people ' + verbOne + ' horribly in one of his ' + adjectiveThree + ' schemes. He drinks too much ' + beverageOne + ' and I constantly have to clean up his ' + pluralNounTwo + '!' + '.</p>' 
+    var paragraph = '<p> Rick is the smartest ' + nounOne + ' in the galaxy, but he\'s also the most ' + adjectiveOne + '. He uses his intelligence for all the ' + adjectiveTwo + ' reasons, such as turning himself into a talking ' + foodOne + ' to avoid going to (the) ' + placeOne + ' with his family. He won\'t come to the rescue when some ' + pluralNounOne + ' are nibbling on my ' + bodyPartOne + ' unless there\'s something in it for him, and he definitely won\'t care if people ' + verbOne + ' horribly in one of his ' + adjectiveThree + ' schemes. He drinks too much ' + beverageOne + ' and I constantly have to clean up his ' + pluralNounTwo+ '!</p>' 
 
     document.getElementById("answer").innerHTML = paragraph;
 }
